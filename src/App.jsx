cf28daea88729781d789906d430c95072c235777
Lucas Alcocer <lucasalcocer174@gmail.com>
2024-06-19 19:11:05 -0300

    modiciacion menor en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import Formulario from './componentes/Formulario';
+import SoporteNotas from './componentes/SoporteNotas';
 
 function App() {
   const [count, setCount] = useState(0)
